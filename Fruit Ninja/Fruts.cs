d15e38afb41ff17e13c1c3115e951cdcd71f710c
Александр Pistak <lutcifer333@gmail.com>
2025-05-06 16:27:50 +0700

    Добавлена новая игра Fruit Ninja!

---
+++ Fruit Ninja/Fruts.cs
@@ -0,0 +1,26 @@
+﻿using Balls_Core;
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Fruit_Ninja
+{
+    public class Fruts : Ball
+    {
+        private float g = 0.2f;
+        public Fruts(Form form, float centerX, float cenetrY) : base(form)
+        {
+            radius = 15;
+            this.centerX = centerX;
+            this.centerY = cenetrY;
+            RandomSizeAndSpeedBalls();
+            BallColor = Color.FromArgb(Random.Next(256), Random.Next(256), Random.Next(256)); // Случайный цвет
+            vy = -Math.Abs(vy);
+        }
+        protected override void Go()
+        {
+            base.Go();
+            vy += g;
+        }
+    }
+}
---
+++ Fruit Ninja/MainForm.cs
@@ -0,0 +1,98 @@
+﻿using Balls_Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Fruit_Ninja
+{
+
+    public partial class MainForm : Form
+    {
+        private List<Ball> listFruts;
+        private Timer timer;
+        private Timer spawnTimer;
+        private Point lastMousePosition;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            listFruts = new List<Ball>();
+            DoubleBuffered = true;
+
+            // Настройка таймера анимации
+            timer = new Timer();
+            timer.Interval = 15;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            // Настройка таймера создания шариков
+            spawnTimer = new Timer();
+            spawnTimer.Interval = 1500; // Создаем новый шарик каждый n
+            spawnTimer.Tick += Spawn_Tick;
+            spawnTimer.Start();
+
+            MouseMove += MainForm_MouseMove;
+            lastMousePosition = Cursor.Position; // Инициализируем последнюю позицию мыши
+        }
+        private void MainForm_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            foreach (var frut in listFruts)
+            {
+                if (frut is MoveFruts moveFrut && !moveFrut.HasCut)
+                {
+                    moveFrut.CheckCut(e.X, e.Y);
+                }
+            }
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Обновляем позиции всех шаров
+            foreach (var frut in listFruts)
+            {
+                frut.Move();
+            }
+
+            var frutsToRemove = new List<Ball>();
+
+            foreach (var frut in listFruts)
+            {
+                if (frut is MoveFruts moveFrut)
+                {
+                    if (moveFrut.HasCut)
+                    {
+                        frutsToRemove.Add(frut);
+                    }
+                    else if (frut.IsOutOfBounds())
+                    {
+                        frutsToRemove.Add(frut);
+                    }
+                }
+            }
+            foreach (var frut in frutsToRemove)
+            {
+                listFruts.Remove(frut);
+            }
+
+            Invalidate();
+        }
+        private void Spawn_Tick(object sender, EventArgs e)
+        {
+            var frut = new MoveFruts(this);
+            listFruts.Add(frut);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias; // Сглаживание
+            foreach (var frut in listFruts)
+            {
+                frut.Show(e.Graphics);
+            }
+        }
+    }
+}
